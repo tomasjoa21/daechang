a2ea6b4ac2eade64449fc18a9e30b46295afd58e
ingglobal <ing@ingglobal.net>
2023-06-16 16:03:30 +0900

    j_md

---
+++ adm/v10/dashboard/output.php
@@ -0,0 +1,154 @@
+<?php
+include_once('./_common.php');
+
+$g5['title'] = '생산보고서';
+include_once('./_head.sub.php');
+
+
+if(is_file(G5_USER_ADMIN_PATH.'/'.$g5['dir_name'].'/css/style.css')) {
+    add_stylesheet('<link rel="stylesheet" href="'.G5_USER_ADMIN_URL.'/'.$g5['dir_name'].'/css/style.css">', 2);
+}
+if(is_file(G5_USER_ADMIN_PATH.'/'.$g5['dir_name'].'/css/'.$g5['file_name'].'.css')) {
+    add_stylesheet('<link rel="stylesheet" href="'.G5_USER_ADMIN_URL.'/'.$g5['dir_name'].'/css/'.$g5['file_name'].'.css">', 2);
+}
+?>
+<style>
+</style>
+
+<div class="report_wrapper">
+    <div class="report_container">
+
+            <div class="div_title_02f"><i class="fa fa-check" aria-hidden="true"> 일자별 생산</i></div>
+            <div id="chart_day"></div>
+            <div class="div_info_body">
+
+                <table class="table01">
+                    <thead class="tbl_head">
+                    <tr>
+                        <th scope="col" style="width:100px;">구분</th>
+                        <th scope="col" style="width:15%">생산</th>
+                        <th scope="col" style="width:15%;">OK</th>
+                        <th scope="col" style="width:15%;">NG</th>
+                        <th scope="col" style="width:15%;">불량율</th>
+                    </tr>
+                    </thead>
+                    <tbody class="tbl_body">
+                    <?php
+                    $sql = "SELECT (CASE WHEN n='1' THEN ymd_date ELSE 'total' END) AS item_name
+                                , SUM(output_total) AS output_total
+                                , MAX(output_total) AS output_max
+                                , SUM(output_good) AS output_good
+                                , SUM(output_defect) AS output_defect
+                            FROM
+                            (
+                 
+                                SELECT 
+                                    ymd_date
+                                    , SUM(output_total) AS output_total
+                                    , SUM(output_good) AS output_good
+                                    , SUM(output_defect) AS output_defect
+                                FROM
+                                (
+                                    (
+                                    SELECT 
+                                        CAST(ymd_date AS CHAR) AS ymd_date
+                                        , 0 AS output_total
+                                        , 0 AS output_good
+                                        , 0 AS output_defect
+                                    FROM {$g5['ymd_table']} AS ymd
+                                    WHERE ymd_date BETWEEN '".$st_date."' AND '".$en_date."'
+                                    ORDER BY ymd_date
+                                    )
+                                    UNION ALL
+                                    (
+                                    SELECT 
+                                        work_date AS ymd_date
+                                        , COUNT(xry_idx) AS output_total
+                                        , SUM( CASE WHEN result = 'OK' THEN 1 ELSE 0 END ) AS output_good
+                                        , SUM( CASE WHEN result = 'NG' THEN 1 ELSE 0 END ) AS output_defect
+                                    FROM g5_1_xray_inspection
+                                    WHERE work_date >= '".$st_date."' AND work_date <= '".$en_date."'
+                                    GROUP BY ymd_date
+                                    ORDER BY ymd_date
+                                    )
+                                ) AS db_table
+                                GROUP BY ymd_date
+
+                            ) AS db2, g5_5_tally AS db_no
+                            WHERE n <= 2
+                            GROUP BY item_name
+                            ORDER BY n DESC, item_name
+                    ";
+                    // echo $sql.'<br>';
+
+                    // // 전체 목표가 아니고 날짜별 목표중에서 최고값 추출
+                    // $result = sql_query($sql,1);
+                    // for ($i=0; $row=sql_fetch_array($result); $i++) {
+                    //     // 합계인 경우는 건너뛰고
+                    //     if($row['item_name'] != 'total') {
+                    //         $item_target[] = $target['date'][preg_replace("/-/","",$row['item_name'])];
+                    //     }
+                    // }
+                    // // echo max($item_target).'<br>';
+                    
+                    $result = sql_query($sql,1);
+                    for ($i=0; $row=sql_fetch_array($result); $i++) {
+                        //print_r2($row);
+                        // 합계인 경우
+                        if($row['item_name'] == 'total') {
+                            $row['item_name'] = '합계';
+                            $row['tr_class'] = 'tr_stat_total';
+                            $output_total = $row['output_total'];
+                            $output_max = $row['output_max']; // 값들 중에서 최대값
+                        }
+                        else {
+                            $row['tr_class'] = 'tr_stat_normal';
+                            $categories[] = $row['item_name'];
+                            $series_ok[] = $row['output_good'];
+                            $series_ng[] = $row['output_defect'];
+                        }
+                        // echo $output_total.'<br>';
+                        // echo $output_max.'<br>';
+
+                        // 비율
+                        $row['rate'] = ($output_total) ? $row['output_total'] / $output_total * 100 : 0 ;
+                        $row['ng_rate'] = ($row['output_defect']) ? $row['output_defect'] / $row['output_total'] * 100 : 0 ;
+
+                        // First line total skip, start from second line.
+                        // if($i>0) {
+                            echo '
+                            <tr class="'.$row['tr_class'].'">
+                                <td class="text_left">'.$row['item_name'].'</td>
+                                <td class="text_right pr_5">'.number_format($row['output_total']).'</td><!-- 생산 -->
+                                <td class="text_right pr_5">'.number_format($row['output_good']).'</td><!-- 양호 -->
+                                <td class="text_right pr_5">'.number_format($row['output_defect']).'</td><!-- 불량 -->
+                                <td class="text_right pr_5">'.round($row['ng_rate'],2).' %</td><!-- 불량율 -->
+                            </tr>
+                            ';
+                        // }
+                    }
+                    if ($i == 0)
+                        echo '<tr class="tr_empty"><td class="td_empty" colspan="6">자료가 없습니다.</td></tr>';
+                    ?>
+                </tbody>
+                </table>
+            </div><!-- .div_info_body -->    
+
+    </div>
+    <div class="report_container">
+        <div class="report_left">
+        
+        </div>
+        <div class="report_right">
+        
+        </div>
+    </div>
+
+</div>
+
+
+
+
+<?php
+include_once ('./_tail.sub.php');
+?>
--- adm/v10/index.php
+++ adm/v10/index.php
@@ -9,6 +9,12 @@ include_once ('./_head.php');
 
 $demo = 0;  // 데모인 경우 1로 설정하세요. (packery 박스가 맨 위에 떠 있어서 디버깅 데이터를 가려버리네요.)
 // h1=84, h2=93.9%, h3=95.9%, h4=97%, h5=97.6%, h6=98%
+$cont_head = '
+<div class="widget_title">
+    <span>생산량</span>
+    <a href="javascript:" class="chart_setting"><i class="fa fa-gear"></i></a>
+</div>
+';
 $cont = '
 <div class="widget_title">
     <span>생산량</span>
@@ -17,23 +23,25 @@ $cont = '
 <div class="widget_content">content</div>
 ';
 // $cont = trim($cont, "\n");
+$cont_head = str_replace("\n", "", $cont_head);
 $cont = str_replace("\n", "", $cont);
 ?>
 <script src="<?=G5_USER_URL?>/temp/node_modules/gridstack/dist/gridstack-all.js"></script>
 <link href="<?=G5_USER_URL?>/temp/node_modules/gridstack/dist/gridstack.min.css" rel="stylesheet"/>
 <link href="<?=G5_USER_URL?>/temp/node_modules/gridstack/dist/gridstack-extra.min.css" rel="stylesheet"/>
 <style type="text/css">
-  /* .grid-stack { background: #FAFAD2; } */
-  .grid-stack-item-content { background-color: #1f1f20; }
-  .grid-stack>.grid-stack-item>.grid-stack-item-content {overflow-y: hidden;}
-  .widget_title {position:relative;padding:2px 4px 3px;background-color:#1d263a;border-bottom:solid 2px #040816;}
-  .widget_title .chart_setting {position:absolute;right:5px;top:0;}
-  .widget_content {padding:2px 4px 3px;height:84.3%;border:solid 0px red;}
-  .grid-stack-item[gs-h="2"] .widget_content {height:92.5%;}
-  .grid-stack-item[gs-h="3"] .widget_content {height:95.1%;}
-  .grid-stack-item[gs-h="4"] .widget_content {height:96.3%;}
-  .grid-stack-item[gs-h="5"] .widget_content {height:97.1%;}
-  .grid-stack-item[gs-h="6"] .widget_content {height:97.5%;}
+/* .grid-stack { background: #FAFAD2; } */
+.grid-stack-item-content { background-color: #1f1f20; }
+.grid-stack>.grid-stack-item>.grid-stack-item-content {overflow-y: hidden;}
+.widget_title {position:relative;padding:2px 4px 3px;background-color:#1d263a;border-bottom:solid 2px #040816;}
+.widget_title .chart_setting {position:absolute;right:5px;top:0;}
+.widget_content {padding:2px 4px 3px;height:84.3%;border:solid 0px red;}
+.grid-stack-item[gs-h="2"] .widget_content {height:92.5%;}
+.grid-stack-item[gs-h="3"] .widget_content {height:95.1%;}
+.grid-stack-item[gs-h="4"] .widget_content {height:96.3%;}
+.grid-stack-item[gs-h="5"] .widget_content {height:97.1%;}
+.grid-stack-item[gs-h="6"] .widget_content {height:97.5%;}
+.widget_content iframe {width:100%;height:100%;}
 </style>
 
 <div class="grid-stack">
@@ -43,16 +51,10 @@ $cont = str_replace("\n", "", $cont);
 
 <script type="text/javascript">
 var items = [
-  {w: 1, h: 1, noResize: true, content: '<?=$cont?>'},
-  {w: 2, h: 2, noResize: true, content: '<?=$cont?>'},
-  {w: 1, h: 1, noResize: true, content: '<?=$cont?>'},
-  {w: 2, h: 2, noResize: true, content: '<?=$cont?>'},
-  {w: 1, h: 2, noResize: true, content: '<?=$cont?>'},
-  {w: 1, h: 1, noResize: true, content: '<?=$cont?>'},
-  {w: 1, h: 1, noResize: true, content: '<?=$cont?>'},
-  {w: 1, h: 1, noResize: true, content: '<?=$cont?>'},
-  {w: 1, h: 1, noResize: true, content: '<?=$cont?>'},
-  {w: 1, h: 1, noResize: true, content: '<?=$cont?>'}
+  {w: 2, h: 1, noResize: true, content: '<?=$cont_head?><div class="widget_content"><iframe id="frame_02" src="<?=G5_USER_ADMIN_URL?>/dashboard/uph.php?w=1&h=1" frameborder="0" scrolling="no"></iframe></div>'},
+  {w: 2, h: 1, noResize: true, content: '<?=$cont_head?><div class="widget_content"><iframe id="frame_02" src="<?=G5_USER_ADMIN_URL?>/dashboard/production.php?w=1&h=1" frameborder="0" scrolling="no"></iframe></div>'},
+  {w: 2, h: 1, noResize: true, content: '<?=$cont_head?><div class="widget_content"><iframe id="frame_02" src="<?=G5_USER_ADMIN_URL?>/dashboard/uph.php?w=1&h=1" frameborder="0" scrolling="no"></iframe></div>'},
+  {w: 5, h: 2, noResize: true, content: '<?=$cont?>'}
 ];
 var options = {
     column:6,
