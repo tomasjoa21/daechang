c6505d37c0e828eaf0d98d2d89c679b86f4f1403
ingglobal <rnd@ingglobal.net>
2023-09-05 11:15:24 +0900

    tmd

--- adm/v10/_top_menu_manager.php
+++ adm/v10/_top_menu_manager.php
@@ -5,11 +5,11 @@ if (!defined('_GNUBOARD_')) exit;
 ${'active_'.$g5['file_name']} = ' btn_top_menu_active';
 
 // 최고관리자인 경우만
-if($member['mb_manager_yn']) {
+// if($member['mb_manager_yn']) {
     $sub_title_list = ' <a href="./config_manager_form.php" class="btn_top_menu '.$active_config_manager_form.'">관리자설정</a>
                         <a href="./config_mms_pos_form.php" class="btn_top_menu '.$active_config_mms_pos_form.'">설비위치설정</a>
     ';
-}
+// }
 
 $g5['container_sub_title'] = '
 <h2 id="container_sub_title">
